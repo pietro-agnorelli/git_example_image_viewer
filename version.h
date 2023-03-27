cd5670593a4b11634fb4291df21c3a7183f239cb
Pietro <pietro.agnorelli@stud.unifi.it>
2023-03-27 17:28:00 +0200

    Added version name

---
+++ version.h
@@ -0,0 +1,12 @@
+//
+// Created by Pietro on 27/03/2023.
+//
+
+#ifndef GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+#define GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+
+#include <string>
+
+static const std::string version = "v1.0";
+
+#endif //GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
